15a15fc1f1f02c5c145bd0e5c854657301b02179
RJ Van Hecke <vanheckeraoul@gmail.com>
2021-07-18 20:14:01 -0400

    styled single-product.php

--- single-product.php
+++ single-product.php
@@ -2,8 +2,30 @@
 
 <?php get_header(); ?>
 
+<div class="single-product__spacer"></div>
 <div class="single-product">
-  <h2><?php the_title(); ?></h2>
+
+  <?php
+    while(have_posts()){
+      the_post(); 
+  ?>
+
+  <div class="single-product__display">
+    <div class="single-product__img-box">
+      <img src="<?php the_field('full_image'); ?>" alt="" class="single-product__img">
+    </div>
+  </div>
+
+  <div class="single-product__info-box">
+    <h2 class="single-product__heading"><?php the_title(); ?></h2>
+    <p class="single-product__description"><?php the_field('description'); ?></p>
+    <div class="single-product__buy-box">
+      <p class="single-product__price">$<?php the_field('price'); ?></p>
+      <a href="<?php the_field('affiliate_link_text'); ?>" class="single-product__buy-link" target="_blank">See on Amazon</a>
+    </div>
+  </div>
+
+  <?php } wp_reset_postdata(); ?>
 
 
 
